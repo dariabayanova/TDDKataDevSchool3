df2f3c868f5c4d202c260eb8e8aa41dce3c69ccd
Dasha Bayanova <d.bayanova@dodopizza.com>
2018-03-17 16:11:39 +0300

    WhenAddStringWithTwoDelimetersBetweenNumbers - Exception was thrown

--- Domain/Calculator.cs
+++ Domain/Calculator.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Domain
 {
     public class Calculator
     {
-        private int zero;
+        private int zero = 0;
         private readonly char[] separators = {',', '\n'};
 
         public int Add(string numbers)
@@ -14,7 +15,17 @@ namespace Domain
                 return zero;
             }
 
-            return numbers.Split(separators).Sum(int.Parse);
+            return numbers.Split(separators).Sum(ParseNumber);
+        }
+
+        private int ParseNumber(string number)
+        {
+            if (!int.TryParse(number, out var result))
+            {
+                throw new ArgumentException($"Couldn't parse number {number}.");
+            }
+
+            return result;
         }
 
         private bool IsEmpty(string value)
---
+++ Tests/WhenAddStringWithTwoDelimetersBetweenNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class WhenAddStringWithTwoDelimetersBetweenNumbers
+    {
+        [Test]
+        public void ExceptionWasThrown()
+        {
+            var calculator = new Calculator();
+
+            Assert.Throws<ArgumentException>(() => calculator.Add("1,\n"));
+        }
+    }
+}
