fadae2cfb36b3e8bc242ecb53d986cb05e466ca0
Dasha Bayanova <d.bayanova@dodopizza.com>
2018-03-17 15:30:54 +0300

    WhenAddEmptyString - CalculatorReturnsZero

---
+++ Tests/WhenAddEmptyString.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class WhenAddEmptyString
+    {
+        [Test]
+        public void CalculatorReturnsZero()
+        {
+            var calculator = new Calculator();
+
+            var sum = calculator.Add("");
+
+            Assert.That(sum, Is.Zero);
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(string value)
+        {
+            return 0;
+        }
+    }
+}
